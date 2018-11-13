98b68a2c2dc164a20dd4926ae51dc4820721ebdc
Nordstock <nordstok@mail.ru>
2018-11-13 23:10:36 +0600

    1
    1

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Безымянная страница</title>
+<meta name="generator" content="WYSIWYG Web Builder 10 - http://www.wysiwygwebbuilder.com">
+<link href="ico.png" rel="shortcut icon">
+<link href="lsg.css" rel="stylesheet" type="text/css">
+<link href="index.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="space"><br></div>
+<div id="container">
+<div id="wb_Image1" style="position:absolute;left:300px;top:263px;width:405px;height:129px;z-index:0;">
+<img src="images/lspecgroup.png" id="Image1" alt=""></div>
+<div id="wb_Image2" style="position:absolute;left:350px;top:490px;width:300px;height:33px;z-index:1;">
+<img src="images/mail.png" id="Image2" alt=""></div>
+</div>
+</body>
+</html>
